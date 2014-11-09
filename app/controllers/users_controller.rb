2b0a435c6e3f65ebdc0f36de5c69fe7372fc2337
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-11-08 23:48:01 -0500

    Add admin field to user. Convert users controller to decent exposure

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,60 +1,44 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!
 
-  # GET /users
-  # GET /users.json
-  def index
-    @users = User.all
+  expose(:users) { User.order('name ASC') }
+
+  expose(:user) do
+    unless params[:id].nil?
+      User.find(params[:id])
+    else
+      User.new
+    end
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
-
-  # GET /users/new
-  def new
-    @user = User.new
-  end
-
-  # GET /users/1/edit
-  def edit
-  end
-
-  # POST /users
-  # POST /users.json
   def create
-    @user = User.new(user_params)
+    self.user = User.new(user_params)
 
     respond_to do |format|
-      if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
-        format.json { render :show, status: :created, location: @user }
+      if user.save
+        format.html { redirect_to user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: user }
       else
         format.html { render :new }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     respond_to do |format|
-      if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
-        format.json { render :show, status: :ok, location: @user }
+      if user.update(user_params)
+        format.html { redirect_to user, notice: 'User was successfully updated.' }
+        format.json { render :show, status: :ok, location: user }
       else
         format.html { render :edit }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
+        format.json { render json: user.errors, status: :unprocessable_entity }
       end
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
-    @user.destroy
+    user.destroy
     respond_to do |format|
       format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
       format.json { head :no_content }
@@ -62,12 +46,7 @@ class UsersController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Never trust parameters from the scary internet, only allow the white list through.
     def user_params
       params.require(:user).permit(:name, :email)
     end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,16 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  before_create :set_admin
+
+  def to_s
+    "#{ name.titleize unless name.nil? }"
+  end
+
+  private
+
+    def set_admin
+      self.admin = false
+    end
 end
