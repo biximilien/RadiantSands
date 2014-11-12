d5a0fff18f3140331fb57ad2c89b67e6a9c65af8
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-11-12 00:40:31 -0500

    Basic registration controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
-  before_action :authenticate_user!
+  
+  before_action :authenticate_user!, only: [ :index, :edit, :destroy ]
 
   expose(:users) { User.order('name ASC') }
 
