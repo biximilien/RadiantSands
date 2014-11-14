9b148baf4aeb48b1b67395a98d9692842274df19
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-11-14 18:08:18 -0500

    Add icons to filters. Can filter none.

--- app/models/event.rb
+++ app/models/event.rb
@@ -100,30 +100,7 @@ class Event < ActiveRecord::Base
   end
 
   def type_icon
-    return 'misc.png' if type.nil?
-    case type.name
-    when 'dance'; 'icons/dance.png'
-    when 'theatre'; 'icons/theatre.png'
-    when 'spoken word / literature'; 'icons/swl.png'
-    when 'visual arts'; 'icons/visual_arts.png'
-    when 'classical'; 'icons/classical.png'
-    when 'pop'; 'icons/pop.png'
-    when 'rock'; 'icons/rock.png'
-    when 'punk'; 'icons/punk.png'
-    when 'hip hop'; 'icons/hip_hop.png'
-    when 'r&b'; 'icons/rnb.png'
-    when 'reggae'; 'icons/reggae.png'
-    when 'blues'; 'icons/blues.png'
-    when 'jazz'; 'icons/jazz.png'
-    when 'electronic music'; 'icons/electronic_music.png'
-    when 'alternative'; 'icons/alternative.png'
-    when 'experimental'; 'icons/experimental.png'
-    when 'world'; 'icons/world.png'
-    when 'mixed media'; 'icons/mixed_media.png'
-    
-    else
-      'icons/misc.png'
-    end
+    type.icon
   end
 
 
--- app/models/event_type.rb
+++ app/models/event_type.rb
@@ -5,6 +5,33 @@ class EventType < ActiveRecord::Base
     "#{ name.titleize unless name.nil? }"
   end
 
+  def icon
+    return 'misc.png' if name.nil?
+    case name
+    when 'dance'; 'icons/dance.png'
+    when 'theatre'; 'icons/theatre.png'
+    when 'spoken word / literature'; 'icons/swl.png'
+    when 'visual arts'; 'icons/visual_arts.png'
+    when 'classical'; 'icons/classical.png'
+    when 'pop'; 'icons/pop.png'
+    when 'rock'; 'icons/rock.png'
+    when 'punk'; 'icons/punk.png'
+    when 'hip hop'; 'icons/hip_hop.png'
+    when 'r&b'; 'icons/rnb.png'
+    when 'reggae'; 'icons/reggae.png'
+    when 'blues'; 'icons/blues.png'
+    when 'jazz'; 'icons/jazz.png'
+    when 'electronic music'; 'icons/electronic_music.png'
+    when 'alternative'; 'icons/alternative.png'
+    when 'experimental'; 'icons/experimental.png'
+    when 'world'; 'icons/world.png'
+    when 'mixed media'; 'icons/mixed_media.png'
+    
+    else
+      'icons/misc.png'
+    end
+  end
+
   private
     def to_lower_case
       self.name = name.downcase unless name.nil?
