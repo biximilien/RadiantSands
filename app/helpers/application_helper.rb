cc7c5fef198848ddfb825f7b59e18baa0f6fdab2
Maxime Gauthier <maxime.gauthier88@gmail.com>
2014-12-04 18:12:36 -0500

    Prettify error messages

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -9,4 +9,11 @@ module ApplicationHelper
       #{out}
     </ul>\n".html_safe
   end
+
+   def bootstrap_class_for flash_type
+    { success: "alert-success",
+      error: "alert-danger",
+      alert: "alert-warning",
+      notice: "alert-info" }[flash_type.to_sym] || flash_type.to_s
+  end 
 end
