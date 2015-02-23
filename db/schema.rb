1c67094cbf95f442fa9b91448a6b7b48decfba59
Maxime 'biximilien' Gauthier <maxime.gauthier88@gmail.com>
2015-02-23 02:24:58 -0500

    Fix database

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219034256) do
+ActiveRecord::Schema.define(version: 20150223065841) do
 
   create_table "ads", force: true do |t|
     t.datetime "created_at"
@@ -70,14 +70,10 @@ ActiveRecord::Schema.define(version: 20150219034256) do
     t.string   "day_of_week",   default: "",      null: false
     t.string   "type",          default: "Event", null: false
     t.integer  "list_id"
-    t.index ["day_of_week"], :name => "index_events_on_day_of_week"
-    t.index ["uid"], :name => "index_events_on_uid", :unique => true
   end
 
-  create_table "events_event_lists", force: true do |t|
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+  add_index "events", ["day_of_week"], name: "index_events_on_day_of_week"
+  add_index "events", ["uid"], name: "index_events_on_uid", unique: true
 
   create_table "lists", force: true do |t|
     t.datetime "created_at"
@@ -124,9 +120,10 @@ ActiveRecord::Schema.define(version: 20150219034256) do
     t.string   "provider"
     t.string   "uid"
     t.integer  "user_group_id"
-    t.index ["email"], :name => "index_users_on_email", :unique => true
-    t.index ["reset_password_token"], :name => "index_users_on_reset_password_token", :unique => true
   end
+
+  add_index "users", ["email"], name: "index_users_on_email", unique: true
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
   create_table "venues", force: true do |t|
     t.string   "name"
